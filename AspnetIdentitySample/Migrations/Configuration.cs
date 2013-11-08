aa7f71954e63b083f34ed9aa429d7a587e39b4db
Wouter Dierckx <wouter.dierckx@student.ap.be>
2013-11-08 14:57:16 +0100

    Migrations

--- AspnetIdentitySample/Migrations/Configuration.cs
+++ AspnetIdentitySample/Migrations/Configuration.cs
@@ -1,31 +1,52 @@
 namespace Examonitor.Migrations
 {
+    using Examonitor.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Examonitor.Models.MyDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Examonitor.Models.ExamenMonitorDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(Examonitor.Models.MyDbContext context)
+        protected override void Seed(Examonitor.Models.ExamenMonitorDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+             context.MonitorBeurt.AddOrUpdate(i => i.MonitorBeurtId,
+                new MonitorBeurtModel
+                {
+                    Datum = DateTime.Parse("2013/10/18"),
+                    Start = "8:30",
+                    Einde = "10:30",
+                    Duurtijd = "2:30",
+                    Capaciteit = 2,
+                    Gereserveerd = 0,
+                    AangemaaktOp = DateTime.Parse("2013/10/18"),
+                    Aangepast = DateTime.Parse("2013/10/18"),
+                    Soort = "Digitaal",
+                    Campus = "BouwmeesterStraat",
+                    Departement = "ONDERWIJS EN TRAINING"
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+                },
+                new MonitorBeurtModel
+                {
+                    Datum = DateTime.Parse("2013/10/18"),
+                    Start = "8:30",
+                    Einde = "10:30",
+                    Duurtijd = "2:30",
+                    Capaciteit = 2,
+                    Gereserveerd = 0,
+                    AangemaaktOp = DateTime.Parse("2013/10/18"),
+                    Aangepast = DateTime.Parse("2013/10/18"),
+                    Soort = "Digitaal",
+                    Campus = "Meistraat",
+                    Departement = "IT"
+
+                });
+       
         }
     }
 }
