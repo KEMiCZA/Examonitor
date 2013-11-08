348fcb1b48fc2ea34a5f0883cd3878d2b4dc0cce
Wouter Dierckx <wouter.dierckx@student.ap.be>
2013-11-08 14:38:44 +0100

    MonitorBeurt2

---
+++ AspnetIdentitySample/Models/ExamenMonitorDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Examonitor.Models
+{
+    public class ExamenMonitorDbContext : MyDbContext
+    {
+        public DbSet<MonitorBeurtModel> MonitorBeurt { get; set; }
+        public DbSet<ReservatieModel> Reservatie { get; set; }
+    }
+}
---
+++ AspnetIdentitySample/Models/MonitorBeurtModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Examonitor.Models
+{
+    public class MonitorBeurtModel
+    {
+
+        
+        [Key] public int MonitorBeurtId { get; set; }
+        [Display(Name = "Aangemaakt Op")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Datum { get; set; }
+        public string Start { get; set; }
+        public string Einde { get; set; }
+        public string Duurtijd { get; set; }
+        public int Capaciteit { get; set; }
+        public int Gereserveerd { get; set; }
+        [Display(Name = "Aangemaakt Op")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime AangemaaktOp { get; set; }
+        [Display(Name = "Aangepast Op")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Aangepast { get; set; }
+        public string Soort { get; set; }
+        public string Campus { get; set; }
+        public string Departement { get; set; }
+    }
+}
