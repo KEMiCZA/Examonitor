73f33c28754936c373acd17e68969744749ce270
Hans Schouwaerts <hans.schouwaerts@student.ap.be>
2013-12-13 14:50:30 +0100

    Remove ToDo model/controller/views

--- AspnetIdentitySample/Models/AppModel.cs
+++ AspnetIdentitySample/Models/AppModel.cs
@@ -14,7 +14,6 @@ namespace Examonitor.Models
         [Required]
         [EmailAddress]
         public string Email { get; set; }
-        public virtual ICollection<ToDo> ToDoes { get; set; }
     }
 
     public class Campus
@@ -24,13 +23,6 @@ namespace Examonitor.Models
         public string Name { get; set; }
     }
 
-    public class ToDo
-    {
-        public int Id { get; set; }
-        public string Description { get; set; }
-        public bool IsDone { get; set; }
-        public virtual MyUser User { get; set; }
-    }
     public class MyDbContext : IdentityDbContext<MyUser>
     {
         public MyDbContext()
@@ -47,7 +39,6 @@ namespace Examonitor.Models
                 .ToTable("Users");
         }
 
-        public DbSet<Examonitor.Models.ToDo> ToDoes { get; set; }
         public DbSet<MonitorBeurtModel> MonitorBeurt { get; set; }
         public DbSet<ReservatieModel> Reservatie { get; set; }
         public DbSet<Campus> Campus { get; set; }
